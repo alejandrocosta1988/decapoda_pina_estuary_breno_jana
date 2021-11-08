87a0fc13a98faae31317b8c969ddba4c6917fad1
Alejandro Costa <alejandroesfc@gmail.com>
2021-11-08 11:37:28 -0300

    densidade total: boxplot para turno e profundidade. Plotagem com gridArrange. Dois graficos em linha unica

--- plotagem_de_dados_univariados.R
+++ plotagem_de_dados_univariados.R
@@ -10,14 +10,24 @@ dados_para_plotagem <- decapoda %>%
 y_label = bquote('Biovolume'~(mm^-3~m^-3))
 
 biovolume_profundidade <- ggplot(dados_para_plotagem, aes(Profundidade, biovolume, fill = Turno))+
-  geom_boxplot(alpha = .6)+
+  geom_boxplot(alpha = .6, show.legend = FALSE)+
   coord_trans(y = 'log10')+
   scale_y_continuous(breaks = c(10, 50, 100, 250, 500, 1000, 2000))+
   scale_fill_brewer(palette = "Dark2")+
   labs(y = y_label)+
   tema_boxplot
+# Dia - esverdeado; Noite - avermelhado
 
+y_label = bquote('Densidade '~(ind~m^-3))
 
+densidade_profundidade <- ggplot(dados_para_plotagem, aes(Profundidade, Densidade_ind.m3, fill = Turno))+
+  geom_boxplot(alpha = .6, show.legend = FALSE)+
+  scale_fill_brewer(palette = "Dark2")+
+  scale_y_continuous(limits = c(0, 350))+
+  labs(y = y_label)+
+  tema_boxplot
+# Dia - esverdeado; Noite - avermelhado
 
-
+library(gridExtra)
+grid.arrange(densidade_profundidade, biovolume_profundidade, nrow=1)
   
