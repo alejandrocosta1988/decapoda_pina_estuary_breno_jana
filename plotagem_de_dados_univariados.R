356074928b5635c76215ed55a94f4681a0b2f121
Alejandro Costa <alejandroesfc@gmail.com>
2021-11-08 16:08:16 -0300

    boxplot quadruplo com grid.Arrange

--- plotagem_de_dados_univariados.R
+++ plotagem_de_dados_univariados.R
@@ -35,21 +35,22 @@ grid.arrange(densidade_profundidade, biovolume_profundidade, nrow=1)
 
 # Período sazonal
 y_label = bquote('Densidade '~(ind~m^-3))
+x_label = "Período"
 
 densidade_periodo <- ggplot(dados_para_plotagem, aes(Periodo, Densidade_ind.m3))+
-  geom_boxplot(alpha = .6, fill = 'steelblue', width = .5)+
+  geom_boxplot(alpha = .6, fill = 'steelblue', width = .4)+
   scale_y_continuous(limits = c(0, 350))+
-  labs(y = y_label)+
+  labs(y = y_label, x=x_label)+
   tema_boxplot
 
 
 y_label = bquote('Biovolume'~(mm^-3~m^-3))
 biovolume_periodo <- ggplot(dados_para_plotagem, aes(Periodo, biovolume))+
-  geom_boxplot(alpha = .6, fill = 'steelblue', width = .5)+
+  geom_boxplot(alpha = .6, fill = 'steelblue', width = .4)+
   coord_trans(y = 'log10')+
   scale_y_continuous(breaks = c(10, 50, 100, 250, 500, 1000, 2000))+
   scale_fill_brewer(palette = "Dark2")+
-  labs(y = y_label)+
+  labs(y = y_label, x=x_label)+
   tema_boxplot
 
-grid.arrange(densidade_periodo, biovolume_periodo, nrow=1)
+grid.arrange(densidade_profundidade, biovolume_profundidade, densidade_periodo, biovolume_periodo, nrow=2)
